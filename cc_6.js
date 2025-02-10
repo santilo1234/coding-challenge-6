6c9a17cfe1fa35eec455b901e9773bd80894e3bc
santiago lopez <slopezarias@usf.edu>
2025-02-09 22:18:43 -0500

    Task 2- Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -8,3 +8,20 @@ console.log(`Total Profit: $${profit}`);
 
 calculateProfit(20, 30, 100);
 calculateProfit(50, 70, 200);
+
+
+//Task 2- Sales Tax Computation
+
+const calculateSalesTax = function(amount, taxRate){
+    const salesTax = amount * taxRate;
+    console.log(`Sales Tax: $${salesTax}`);
+    
+    };
+    calculateSalesTax(100, 0.07);
+    calculateSalesTax(500,0.1);
+    
+    
+   
+
+    
+
